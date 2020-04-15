fff9e1f73f34eb82c0bb3bb52b22c70cf9e33b7a
emelinegyt <guyetemeline@gmail.com>
2020-04-15 09:44:35 +0200

    grid

--- modal.js
+++ modal.js
@@ -1,15 +1,15 @@
 // create references to the modal...
     var modal = document.getElementById('myModal');
     // to all images -- note I'm using a class!
-    var images = document.getElementsByClassName('myImages');
+    var pictures = document.getElementsByClassName('myImages');
     // the image in the modal
     var modalImg = document.getElementById("img01");
     // and the caption in the modal
     var captionText = document.getElementById("caption");
   
     // Go through all of the images with our custom class
-    for (var i = 0; i < images.length; i++) {
-      var img = images[i];
+    for (var i = 0; i < pictures.length; i++) {
+      var img = pictures[i];
       // and attach our click listener for this image.
       img.onclick = function (evt) {
         console.log(evt);
